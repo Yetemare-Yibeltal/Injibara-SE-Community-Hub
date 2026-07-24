4c40698743b7ce7ba873a26af83544ed9c57fde8
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-25 01:48:17 +0300

    add ratelimiter file

---
+++ server/src/middlewares/rateLimiter.middleware.ts
@@ -0,0 +1,13 @@
+import rateLimit from "express-rate-limit";
+
+export const authRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many login attempts. Please try again in 15 minutes.",
+  },
+  skipSuccessfulRequests: true,
+});
