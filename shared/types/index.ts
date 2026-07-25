7af1b54075843b28bae9d2534f1dc11767d3d4f7
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-25 10:51:14 +0300

    add shared types barrel export

---
+++ shared/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./user.types";
+export * from "./course.types";
+export * from "./chat.types";
+export * from "./message.types";
+export * from "./file.types";
+export * from "./announcement.types";
+export * from "./notification.types";
