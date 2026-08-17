c0e51bf8189394f9b50b62a8b4f988a80e6b6519
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-08-17 23:20:35 +0300

    update utils api response

--- server/src/utils/apiResponse.util.ts
+++ server/src/utils/apiResponse.util.ts
@@ -6,33 +6,5 @@ interface SuccessResponse<T> {
   data?: T;
 }
 
-interface ErrorResponse {
-  success: false;
-  message: string;
-}
 
-export function sendSuccess<T>(
-  res: Response,
-  statusCode: number,
-  message: string,
-  data?: T,
-): Response {
-  const body: SuccessResponse<T> = {
-    success: true,
-    message,
-    ...(data !== undefined ? { data } : {}),
-  };
-  return res.status(statusCode).json(body);
-}
-
-export function sendError(
-  res: Response,
-  statusCode: number,
-  message: string,
-): Response {
-  const body: ErrorResponse = {
-    success: false,
-    message,
-  };
-  return res.status(statusCode).json(body);
-}
+  return res.status(statusCode).json(body)
