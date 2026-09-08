da2f6177225595950d506cac339d69f27ea3fe3a
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-09 02:57:54 +0300

    add chat routes to app router

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -4,6 +4,7 @@ import AppLayout from '../components/layout/AppLayout';
 import LandingPage from '../pages/LandingPage';
 import LoginPage from '../features/auth/LoginPage';
 import DashboardPage from '../pages/DashboardPage';
+import ChatsPage from '../pages/ChatsPage';
 import NotFoundPage from '../pages/NotFoundPage';
 
 export default function AppRouter() {
@@ -16,6 +17,8 @@ export default function AppRouter() {
         <Route element={<ProtectedRoute />}>
           <Route element={<AppLayout />}>
             <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/chats" element={<ChatsPage />} />
+            <Route path="/chats/:chatId" element={<ChatsPage />} />
           </Route>
         </Route>
 
