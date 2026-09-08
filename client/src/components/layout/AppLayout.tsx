4c91c858121cb257218e4cfd0a6455cc1dfdccda
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-08 08:45:53 +0300

    add applayout

---
+++ client/src/components/layout/AppLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
+export default function AppLayout() {
+  return (
+    <div className="flex min-h-screen bg-gray-950">
+      <Sidebar />
+      <div className="flex-1 flex flex-col min-w-0">
+        <Navbar />
+        <main className="flex-1 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
