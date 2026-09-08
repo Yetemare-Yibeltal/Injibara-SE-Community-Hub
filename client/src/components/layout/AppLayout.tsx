8cbd8d9df9401558997820c2f43f53765dc1ebeb
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-09 02:57:54 +0300

    connect socket on authenticated layout mount

--- client/src/components/layout/AppLayout.tsx
+++ client/src/components/layout/AppLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
+import { useSocketConnection } from '../../hooks/useSocketConnection';
 
 export default function AppLayout() {
+  useSocketConnection();
+
   return (
     <div className="flex min-h-screen bg-gray-950">
       <Sidebar />
