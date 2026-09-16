9625e9813f03b69de74edbf8625748691306b04a
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-16 06:01:18 +0300

    update shared index file

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -1,3 +1,6 @@
 export * from "./user.types";
 export * from "./chat.types";
 export * from "./message.types";
+export * from "./file.types";
+export * from "./course.types";
+export * from "./notification.types";
