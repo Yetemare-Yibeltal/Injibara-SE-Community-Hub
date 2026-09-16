61564da56934b87374afbd6ea63b9951ffdf92cc
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-16 06:18:00 +0300

    wire groups files notifications courses profile routes

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -5,6 +5,12 @@ import LandingPage from '../pages/LandingPage';
 import LoginPage from '../features/auth/LoginPage';
 import DashboardPage from '../pages/DashboardPage';
 import ChatsPage from '../pages/ChatsPage';
+import GroupList from '../features/groups/GroupList';
+import FileList from '../features/files/FileList';
+import NotificationList from '../features/notifications/NotificationList';
+import CourseList from '../features/courses/CourseList';
+import CourseDetail from '../features/courses/CourseDetail';
+import ProfilePage from '../features/profile/ProfilePage';
 import NotFoundPage from '../pages/NotFoundPage';
 
 export default function AppRouter() {
@@ -19,6 +25,12 @@ export default function AppRouter() {
             <Route path="/dashboard" element={<DashboardPage />} />
             <Route path="/chats" element={<ChatsPage />} />
             <Route path="/chats/:chatId" element={<ChatsPage />} />
+            <Route path="/groups" element={<GroupList />} />
+            <Route path="/files" element={<FileList />} />
+            <Route path="/notifications" element={<NotificationList />} />
+            <Route path="/courses" element={<CourseList />} />
+            <Route path="/courses/:courseId" element={<CourseDetail />} />
+            <Route path="/profile" element={<ProfilePage />} />
           </Route>
         </Route>
 
